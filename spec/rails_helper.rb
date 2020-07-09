efd28feb31acb286a16a7076b114adc8a371409a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-09 23:49:08 +0100

    Move pry in rspec to support file
    Our pattern is all additional tools or dependencies needed for testing we handle in separate support files. That way we find it easier to ensure our spec and rails helper files stay consistent across projects. It also makes it clearer and easier to see just what additional tools a test suite is using.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,9 +10,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
-# Support debugging in the tests
-require "pry-byebug"
-
 # Require this here to get factories to play nice with engine
 # require "factory_bot_rails"
 
