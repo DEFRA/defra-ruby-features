9427b869e10a45c4f580affda191044e1a0b1956
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-14 00:53:47 +0100

    Add support for rspec test generators

--- lib/defra_ruby_features/engine.rb
+++ lib/defra_ruby_features/engine.rb
@@ -5,5 +5,9 @@ require_relative "configuration"
 module DefraRubyFeatures
   class Engine < ::Rails::Engine
     isolate_namespace DefraRubyFeatures
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
