abff340af1cdec7a68a070e92a41dcbb5602e357
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-14 00:52:08 +0100

    Add something to config test
    There was a configuration_spec.rb. Might as well add something to it!

--- spec/lib/defra_ruby_mocks/configuration_spec.rb
+++ spec/lib/defra_ruby_mocks/configuration_spec.rb
@@ -4,5 +4,10 @@ require "rails_helper"
 
 module DefraRubyFeatures
   RSpec.describe Configuration do
+    let(:subject) { described_class.new }
+
+    it "sets the appropriate default config settings" do
+      expect(subject.feature_toggle_model_name).to be_nil
+    end
   end
 end
